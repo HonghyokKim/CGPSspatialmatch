572cdc54a3d9f905188d1552c7e7195110ce7734
Honghyok Kim <68622296+HonghyokKim@users.noreply.github.com>
2021-11-04 23:05:37 -0400

    Delete R directory

--- R/w.sd.R
+++
@@ -1,6 +0,0 @@
-#' w.sd. function
-#'
-#' This internal function calculates weighted standard deviation
-#' @param x is a variable to be calculated; w is a weight.
-#' w.sd()
-w.sd <- function(x, w) sqrt(sum(((x - w.m(x,w))^2)*w)/(sum(w)*(length(x)-1)/length(x)))
