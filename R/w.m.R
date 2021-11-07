2e0cd2e740a1e7ff863b1be6793758aaf6e75685
Honghyok Kim <68622296+HonghyokKim@users.noreply.github.com>
2021-11-07 01:32:57 -0400

    Delete R directory

--- R/w.m.R
+++
@@ -1,6 +0,0 @@
-#' w.m. function
-#'
-#' This internal function calculates weighted average.
-#' @param x is a variable to be calculated; w is a weight.
-#' w.m()
-w.m <- function(x, w) sum(x*w)/sum(w)
