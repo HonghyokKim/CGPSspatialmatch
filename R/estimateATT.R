ee2643269fec65a5645c9b4f08c751409fe285df
Honghyok Kim <68622296+HonghyokKim@users.noreply.github.com>
2021-11-08 16:05:10 -0500

    Add files via upload

--- R/estimateATT.R
+++ R/estimateATT.R
@@ -299,7 +299,7 @@ estimateATT<-function(dataset,bexp,exp.status=1,cexp,fmethod.replace=TRUE,distbu
     rm(PSerror)
     end.time<-Sys.time()
     print(paste0("Elapsed time: ",round(end.time-start.time,3),attr(end.time-start.time,"units")))
-    return(list(summary.empirical=result,summary=result2,distribution=coefest,
+    return(list(summary.empirical=result,summary=result2,distribution=coefest,modelfit=modelfit,
                 smd.org=smd.org,smd.matched=colMeans(smd.m,na.rm=T),
                 correxp.org=correxp.org,correxp.matched=colMeans(correxp.m,na.rm=T),
                 smd.matched.bs=smd.m,correxp.matched.bs=correxp.m))
