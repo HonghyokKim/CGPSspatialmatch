6d5dad02f6dcfc9242bb03938c2d0336e0021288
Honghyok Kim <68622296+HonghyokKim@users.noreply.github.com>
2021-12-16 22:13:00 -0500

    Add files via upload

---
+++ R/match.caliper.R
@@ -0,0 +1,42 @@
+#' match.caliper function
+#'
+#' This internal function matches units by the generalized propensity score computed by cgpsmatch function. This function is used only within cgpsmatch function.
+#' @export
+#' @examples 
+#' match.caliper()
+match.caliper <-function(data,bexp,gpsname,caliper_bw,exp.status=1,index.status=1,replace2=TRUE) {
+  data[,"GPS"]<-data[,gpsname]
+  num_exp<-length(unique(data[,"strata_matchdist"]))
+  strata<-vector("list",num_exp)
+  PS_caliper <- caliper_bw*sd(data[,"GPS"],na.rm=T)
+  
+  exposed<-data[data[,"index"]==index.status,c("strata_matchdist","GPS")]
+  
+  exposed[,"GPS_range1"] <- exposed[,"GPS"]-PS_caliper
+  exposed[,"GPS_range2"] <- exposed[,"GPS"]+PS_caliper
+  exposed<-exposed[ , -which(names(exposed) %in% c("GPS"))]
+  data<-merge(data,exposed,by="strata_matchdist",all.x=T)
+  
+  data[,"selected"] <- ifelse(data[,"GPS"]>=data[,c("GPS_range1")],1,0) & ifelse(data[,"GPS"]<=data[,c("GPS_range2")],1,0)
+  data<-data[data[,"selected"]==TRUE,]
+  
+  check<-table(data[,c("strata_matchdist","index")])
+  if(ncol(check) !=2) {
+    result<-NULL
+  }
+  else {
+    
+    data<-data[data[,"strata_matchdist"] %!in% as.numeric(names(check[,1][check[,1]==0])),]
+    matched.units.exist<-as.numeric(check[,as.numeric(colnames(check)) != 1])
+    matched.units.exist.order<-ifelse(matched.units.exist>0,1,0)
+    matched.units.exist<-which(matched.units.exist.order==1)
+    result<-data[data[,"strata_matchdist"] %in% matched.units.exist,]
+    result<-result[order(result[,"index"],decreasing=TRUE),]
+    result<-result[order(result[,"strata_matchdist"],rev(result[,"index"])),]
+    
+    
+    ##IF Caliper...
+    #### INDEX 0's weight-> 1/(Number of Index=0)
+    
+  }
+}
---
+++ R/xgb.model.fit.cv.R
@@ -0,0 +1,73 @@
+#' xgb.model.fit.cv function
+#'
+#' This internal function explores optimal parameters in xgb.train using cross-validation
+#' xgb.model.fit.cv()
+
+xgb.model.fit.cv<-function(data,cv.objective,cv.nround,cv.nfold,cv.min.burnin,cv.max_depth,cv.eta,cv.gamma,cv.nthread,cv.subsample,cv.eval_metric,cv.colsample_bytree,cv.min_child_weight,cv.lambda,cv.lambda_bias,cv.alpha,early_stopping_rounds,cv.local.N) {
+  CV.start.time<-Sys.time()
+  hyperparam<-expand.grid(cv.max_depth,cv.eta,cv.gamma,cv.subsample,cv.colsample_bytree,cv.min_child_weight,cv.lambda,cv.lambda_bias,cv.alpha,cv.local.N)
+  colnames(hyperparam)<-c("max_depth","eta","gamma","subsample","colsample_bytree","min_child_weight","lambda","lambda_bias","alpha","cv.local.N")
+  CVrun<-mapply(hyperparam[,1],hyperparam[,2],hyperparam[,3],hyperparam[,4],hyperparam[,5],hyperparam[,6],hyperparam[,7],hyperparam[,8],hyperparam[,9],hyperparam[,10],
+                FUN=function(max_depth,eta,gamma,subsample,colsample_bytree,min_child_weight,lambda,lambda_bias,alpha,cv.local.N) {
+                  param <- list(objective = cv.objective,
+                                max_depth = max_depth,
+                                eta = eta,
+                                gamma = gamma,
+                                nthread=cv.nthread,
+                                eval_metric=cv.eval_metric,
+                                subsample=subsample,
+                                colsample_bytree = colsample_bytree, 
+                                min_child_weight = min_child_weight,
+                                lambda=lambda,
+                                lambda_bias=lambda_bias,
+                                alpha=alpha)
+                  seedn = sample.int(100000, 1)
+                  set.seed(seedn)
+                  cvfit <- xgboost::xgb.cv(data=data, params = param, 
+                                           nfold=cv.nfold, nrounds=cv.nround,
+                                           verbose = F,early_stopping_rounds=early_stopping_rounds,maximize=FALSE)
+                  cvlog<-cvfit$evaluation_log
+                  cvlog<-subset(cvlog,iter>cv.min.burnin)
+                  cvlog<-data.frame(cvlog)
+                  if(cv.eval_metric=="rmse") {
+                    cvlog<-cvlog[cvlog[,4]==min(cvlog[,4]),]
+                  }
+                  
+                  if(cv.eval_metric=="auc") {
+                    cvlog<-cvlog[cvlog[,4]==max(cvlog[,4]),]
+                  }
+                  
+                  return(data.frame(max_depth=max_depth,eta=eta,gamma=gamma,subsample=subsample,
+                                    colsample_bytree=colsample_bytree,min_child_weight=min_child_weight,lambda=lambda,lambda_bias=lambda_bias,alpha=alpha,
+                                    seedn=seedn,iter=cvlog[,1],evalmetric=cvlog[,4]))
+                },
+                
+                SIMPLIFY = FALSE)
+  CV.result<-do.call(rbind,CVrun)
+  if(cv.eval_metric=="rmse") {
+  CV.result<-CV.result[CV.result[,12]==min(CV.result[,12]),]
+  }
+  if(cv.eval_metric=="auc") {
+    CV.result<-CV.result[CV.result[,12]==max(CV.result[,12]),]
+  }
+  
+  print(CV.result)
+  nround = CV.result$iter
+  param <- list(objective = cv.objective,
+                max_depth = CV.result$max_depth,
+                eta = CV.result$eta,
+                gamma = CV.result$gamma,
+                nthread=cv.nthread,
+                subsample=CV.result$subsample,
+                eval_metric=cv.eval_metric,
+                colsample_bytree = CV.result$colsample_bytree, 
+                min_child_weight = CV.result$min_child_weight,
+                lambda = CV.result$lambda,
+                lambda_bias = CV.result$lambda_bias,
+                alpha = CV.result$alpha
+  )
+  set.seed(CV.result$seedn)
+  return(xgboost::xgb.train(data=data, params=param, nrounds=nround))
+  CV.end.time<-Sys.time()
+  print(paste0(round(CV.end.time-CV.start.time,3),attr(CV.end.time-CV.start.time,"units")))
+}
